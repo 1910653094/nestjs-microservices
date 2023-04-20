8d2d2a03ebbe18a814386f5a6e13344079f77eb5
1910653094 <fabian.peyrat01@gmail.com>
2023-04-20 19:00:42 +0200

    bookstore microservice is setup

--- bookstore/src/app.service.ts
+++ bookstore/src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { BookDTO } from './book';
+
+const bookStore: BookDTO[] = [];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getBookById(id: string): BookDTO {
+    return bookStore.find((book: BookDTO) => book.id === id);
+  }
+
+  getAllBooks(): BookDTO[] {
+    return bookStore;
+  }
+
+  createBook(book: Omit<BookDTO, 'id'>): string | null {
+    const existingBook = bookStore.find(
+      (b: BookDTO) =>
+        b.title === book.title &&
+        b.author === book.author &&
+        b.release_date === book.release_date,
+    );
+
+    if (existingBook) {
+      return null;
+    }
+
+    const newID = `Book_${bookStore.length}`;
+    bookStore.push({ ...book, id: newID });
+    return newID;
   }
 }
